db8a8f42e20694c98188f35231b4d40beebd240e
Deepanshi Sharma <deepanshisharma2510@gmail.com>
2022-09-08 13:15:06 +0530

    configure corsheaders

--- src/settings.py
+++ src/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     "organizer.apps.OrganizerConfig",
     
 ]
+CORS_ORIGIN_ALLOW_ALL=True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -63,6 +64,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = 'src.urls'
